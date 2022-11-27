7a0d78f182394ef7d8f6ed1f963925ddd7f6a608
AndriyMartynyuk <andriy.martynyuk.if@gmail.com>
2022-11-27 11:55:39 +0200

    Removed func translate into another module Removed prints. Did it as a result of func work Add structure __name__ = "__main__" If user didn't type a path, then program finished work smoothly

--- sort.py
+++ sort.py
@@ -4,9 +4,11 @@ import os
 import shutil
 import zipfile
 import tarfile
+import translate
 
 
 DIR_PATH = ""
+
 
 EXTENSIONS = {"images": ["jpeg", "png", "jpg", "svg"],
               "video": ["avi", "mp4", "mov", "mpk"],
@@ -14,19 +16,6 @@ EXTENSIONS = {"images": ["jpeg", "png", "jpg", "svg"],
               "audio": ["mp3", "ogg", "wav", "amr"],
               "archives": ["zip", "gz", "tar"],
               "unknown": []}
-
-
-CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
-
-
-TRANS = {}
-
-
-for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
-    TRANS[ord(c)] = l
-    TRANS[ord(c.upper())] = l.upper()
 
 
 def get_folder_name():
@@ -37,30 +26,10 @@ def get_folder_name():
         global DIR_PATH
         DIR_PATH = sys.argv[1]
     else:
-        DIR_PATH = input("Please type a path to the folder you want to clean")
-
-
-if not DIR_PATH:
-    get_folder_name()
-
-
-def normalize(name):
-    """
-    This function normalize file's name
-    """
-    result = ""
-    try:
-        for letter in name.split(".")[0]:
-            if letter.lower() not in CYRILLIC_SYMBOLS and letter.lower() not in TRANSLATION and letter not in "1234567890cwxWXC":
-                result += "_"
-            elif letter.lower() not in CYRILLIC_SYMBOLS:
-                result += letter
-            else:
-                result += TRANS[ord(letter)]
-    except:
-        return name
-    result += "." + name.split(".")[1]
-    return result
+        DIR_PATH = input(
+            "Please type a path to the folder you want to clean: ")
+        if not DIR_PATH:
+            return print(f"You didn't type a path")
 
 
 names_dict = {"images": [],
@@ -69,6 +38,7 @@ names_dict = {"images": [],
               "video": [],
               "archives": [],
               "unknown": []}
+
 
 known_extension = set()
 unknown_extension = set()
@@ -85,14 +55,15 @@ def get_and_rename_files_names(path):
                 for key, values in EXTENSIONS.items():
                     if str(entry).split(".")[1].lower() in values:
                         os.chdir(path_obj)
-                        os.rename(entry.name, normalize(entry.name))
-                        names_dict[key].append(normalize(entry.name))
+                        os.rename(entry.name, translate.normalize(entry.name))
+                        names_dict[key].append(translate.normalize(entry.name))
                         known_extension.add(str(entry).split(".")[1].lower())
                         break
                 if str(entry).split(".")[1].lower() not in known_extension:
                     os.chdir(path_obj)
-                    os.rename(entry.name, normalize(entry.name))
-                    names_dict["unknown"].append(normalize(entry.name))
+                    os.rename(entry.name, translate.normalize(entry.name))
+                    names_dict["unknown"].append(
+                        translate.normalize(entry.name))
                     unknown_extension.add(str(entry).split(".")[1].lower())
             except IndexError as e:
                 names_dict["unknown"].append(entry.name)
@@ -100,9 +71,7 @@ def get_and_rename_files_names(path):
             get_and_rename_files_names(f"{path}\{entry.name}")
 
 
-get_and_rename_files_names(DIR_PATH)
-
-FOLDER_NAMES = list([key for key, value in names_dict.items() if value])
+FOLDER_NAMES = list([key for key in names_dict])
 
 
 def create_folders(path):
@@ -112,19 +81,15 @@ def create_folders(path):
     for key, values in names_dict.items():
         if values:
             for value in values:
-                print("Path ", path, "key ", key,
-                      "value ", value.split('.')[1].lower())
                 os.makedirs(
                     rf"{path}\{key}\{value.split('.')[1].lower()}", exist_ok=True)
-
-
-create_folders(DIR_PATH)
 
 
 def remove_files(path):
     """
     This function remove files to correct directory
     """
+    remove_files_info_logs = []
     counter = 0
     obj_path = Path(path)
     for file in obj_path.iterdir():
@@ -137,12 +102,11 @@ def remove_files(path):
                 try:
                     os.remove(file)
                 except:
-                    print("Folder is not empty")
+                    remove_files_info_logs.append(
+                        f"Folder {file} is not empty")
             elif file.is_file():
                 for key, values in names_dict.items():
                     if file.name in values:
-                        print(file)
-                        print(f'{DIR_PATH}\documents')
                         try:
                             shutil.move(
                                 rf"{file}", rf"{DIR_PATH}\{key}\{str(file).split('.')[1].lower()}")
@@ -155,55 +119,69 @@ def remove_files(path):
                                 rf"{new_name}", rf"{DIR_PATH}\{key}\{str(file).split('.')[1].lower()}")
                             counter += 1
         except Exception as e:
-            print(e)
+            remove_files_info_logs.append(e)
+    return f"Remove files info logs: {remove_files_info_logs}"
 
 
 def deleted_folders(path):
     """
     This func delete empty folders
     """
+    deleted_folders_info_logs = []
     obj_path = Path(path)
     for file in obj_path.iterdir():
         try:
             if file.is_dir():
                 deleted_folders(f'{path}\{file.name}')
-                print(file)
                 os.rmdir(file)
         except:
-            print("Folder is not empty")
+            deleted_folders_info_logs.append(f"Folder {file} is not empty")
+    return f"Deleted folders info logs: {deleted_folders_info_logs}"
 
 
-remove_files(DIR_PATH)
-deleted_folders(DIR_PATH)
-
-existed_archives = set([value.split(".")[1]
-                        for value in names_dict["archives"]])
+existed_archives = set()
 
 
 def unpack_archives(path):
     """
     This func unpach archives
     """
-    for extension in existed_archives:
-        if extension == "zip":
-            for file in os.listdir(rf"{path}\archives\zip"):
-                data_zip = zipfile.ZipFile(rf"{path}\archives\zip\{file}", 'r')
-                os.makedirs(
-                    rf"{path}\archives\zip\{file.split('.')[0]}", exist_ok=True)
-                data_zip.extractall(
-                    path=rf"{path}\archives\zip\{file.split('.')[0]}")
-        elif extension == "tar":
-            try:
-                for file in os.listdir(rf"{path}\archives\tar"):
-                    with tarfile.open(
-                            rf"{path}\archives\tar\{file}", 'r') as data_tar:
-                        os.makedirs(
-                            rf"{path}\archives\tar\{file.split('.')[0]}", exist_ok=True)
-                        data_tar.extractall(
-                            path=rf"{path}\archives\tar\{file.split('.')[0]}")
-            except tarfile.ReadError as e:
-                print("Something wrong with your archive:", e)
+    unpack_archives_info_logs = []
+    try:
+        for extension in existed_archives:
+            if extension == "zip":
+                for file in os.listdir(rf"{path}\archives\zip"):
+                    data_zip = zipfile.ZipFile(
+                        rf"{path}\archives\zip\{file}", 'r')
+                    os.makedirs(
+                        rf"{path}\archives\zip\{file.split('.')[0]}", exist_ok=True)
+                    data_zip.extractall(
+                        path=rf"{path}\archives\zip\{file.split('.')[0]}")
+            elif extension == "tar":
+                try:
+                    for file in os.listdir(rf"{path}\archives\tar"):
+                        with tarfile.open(
+                                rf"{path}\archives\tar\{file}", 'r') as data_tar:
+                            os.makedirs(
+                                rf"{path}\archives\tar\{file.split('.')[0]}", exist_ok=True)
+                            data_tar.extractall(
+                                path=rf"{path}\archives\tar\{file.split('.')[0]}")
+                except tarfile.ReadError as e:
+                    print("Something wrong with your archive:", e)
+    except PermissionError as e:
+        unpack_archives_info_logs.append(e)
+    return f"Unpack archives info logs: {unpack_archives_info_logs}"
 
 
-if existed_archives:
-    unpack_archives(DIR_PATH)
+if __name__ == "__main__":
+    if not DIR_PATH:
+        get_folder_name()
+    if DIR_PATH:
+        get_and_rename_files_names(DIR_PATH)
+        create_folders(DIR_PATH)
+        remove_files(DIR_PATH)
+        deleted_folders(DIR_PATH)
+        existed_archives = set([value.split(".")[1]
+                                for value in names_dict["archives"]])
+        if existed_archives:
+            unpack_archives(DIR_PATH)
